0f2258f8ec92c715c3554c0881fe8060de60001b
GuilhermeRSouza34 <guilhermersouza34@gmail.com>
2025-07-16 23:23:06 -0300

    Importando os Modulo principais do POUI

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { PoModule } from '@po-ui/ng-components';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PoModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
